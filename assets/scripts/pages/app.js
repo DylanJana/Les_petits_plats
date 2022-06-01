58fe00894f36672503c628af0cbb3b9e6eb05cdf
DylanJana <dylan@Dylan.localdomain>
2022-06-01 20:48:54 +0200

    Fin du dynamisme des recettes

--- assets/scripts/pages/app.js
+++ assets/scripts/pages/app.js
@@ -3,6 +3,7 @@ import { fetchRecipesJSON } from "../api/getData.js";
 const wrapperRecipes = document.querySelector(".wrapper");
 
 export let recipes= [];
+export let recipesIngredients = [];
 
 fetchRecipesJSON()
     .then(data =>{
@@ -17,21 +18,41 @@ const addRecipeInDOM = (recipe) =>{
     articleRecipe.classList.add('column', 'card__recipe', 'col-lg-4', 'col-12', 'mb--sm',  'flex', 'flex--column');
     wrapperRecipes.appendChild(articleRecipe);
     articleRecipe.innerHTML=  `
-    <img src="assets/img/dessert.jpg">
+    <img src="assets/img/cook.jpg">
     <div class="card__recipe__body">
-        <div class="row--has-columns-md flex flex-wrap align-items--center mb--xs card__recipe__title">
+        <div class="row--has-columns-md flex flex-wrap mb--xs card__recipe__title">
             <p class="paragraph column col-sm-6 col-12">${recipe.name}</p>
             <span class="column col-sm-6 col-12"><i class="far fa-clock"></i>${recipe.time}min</span>
         </div>
         <div class="row--has-columns-md flex flex-wrap">
             <ul class="column col-sm-6 col-12 recipe__list">
-                <li><span>Lait de coco:</span> 400ml</li>
-                <li><span>Jus de citron:</span> 2</li>
-                <li><span>Créme de coco:</span> 4 cuillères</li>
-                <li><span>Sucre:</span> 20g</li>
-                <li><span>Glaçons:</span> 2</li>
             </ul>
             <p class="paragraph recipe__desc column col-sm-6 col-12">${recipe.description}</p>
         </div>
     </div>`;
+    recipesIngredients = recipe["ingredients"];
+    addRecipeIngredientsInDOM(recipesIngredients);
+}
+
+
+
+const addRecipeIngredientsInDOM = (recipesIngredients) => {
+    let list = document.querySelectorAll('.recipe__list');
+
+    for(let i = 0; i < recipesIngredients.length; i++) {
+        let createLi = document.createElement('li');
+        if((recipesIngredients[i].unit !== undefined) && (recipesIngredients[i].quantity !== undefined)) {
+            createLi.innerHTML = `
+                <li><span>${recipesIngredients[i].ingredient} : </span>${recipesIngredients[i].quantity} ${recipesIngredients[i].unit}</li>
+            `
+        } else if(recipesIngredients[i].quantity !== undefined) {
+            createLi.innerHTML = `
+                <li><span>${recipesIngredients[i].ingredient} : </span>${recipesIngredients[i].quantity}</li>
+            `
+        }
+
+        for(let j = 0; j < list.length; j++) {
+            list[j].appendChild(createLi);
+        }
+    }
 }
